0729d2d9af04a5141bf5298442afc0be963cc133
Miko Kiiski <miko.kiiski@gmail.com>
2014-04-20 18:39:07 +0300

    Cleanup for main.js.

--- js/app/main.js
+++ js/app/main.js
@@ -4,25 +4,25 @@ define(function(require) {
       var $ = require('jquery');
       var _ = require('underscore');
       var hbs = require('handlebars');
-      var upnpDeviceTemplate = require('hbs!./templates/upnp-device')
+      var upnpDeviceTemplate = require('hbs!./templates/upnp-device');
 
       var vera = require('vera');
 
-      var log = function(data) {
-        console.log(data);
-      }
+      var log = function() {
+        console.log(arguments);
+      };
 
       var isUpnpDevice = function(device){
         return device.device_type.indexOf("urn:schemas-upnp-org:device") !== -1;
-      }
+      };
 
       var createDeviceElement = function(device) {
         return upnpDeviceTemplate({ name: device.name, id: device.id });
-      }
+      };
 
       var appendElementToDevicesList = function(deviceElement) {
         $('.devices').append(deviceElement);
-      }
+      };
 
       var showDevices = function(data) {
         $('.devices').empty();
@@ -42,18 +42,9 @@ define(function(require) {
         })
       };
 
-      var showFail = function() {
-        _.each(arguments, function(argument) {
-          console.log(argument);
-        })
-      }
-
       vera.dataRequest("user_data")
         .done(showDevices)
-        .error(showFail);
+        .error(log);
     }
   };
 });
-
-// http://vera/port_3480/data_request
-// http://vera/port_3480/data_request?id=lu_action&output_format=json&DeviceNum=5&serviceId=urn:upnp-org:serviceId:SwitchPower1&action=SetTarget&newTargetValue=1&rand=0.9901279155164957
