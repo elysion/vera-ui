2aec9db4757811eede1a71a8a372b29fecabd0eb
Miko Kiiski <miko.kiiski@gmail.com>
2014-10-25 01:06:12 +0300

    Fix jQuery UI reference in main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ requirejs.config({
     "underscore": "js/lib/underscore/underscore",
     "handlebars": "js/lib/handlebars/handlebars.amd",
     "hbs": "js/lib/require-handlebars-plugin/hbs",
-    "jquery-ui": "js/lib/jquery-ui/ui/jquery-ui"
+    "jquery-ui": "js/lib/jquery-ui/jquery-ui"
   },
   "shim": {
     "underscore": {
